e927300570266c7d68b07ad488bbe0898d806e29
unknown <tupm@devcamp.edu.vn>
2025-01-16 05:28:47 +0700

    update

--- components/Home/About/About.tsx
+++ components/Home/About/About.tsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 const About = () => {
   return (
     <div className="pt-16 pb-16 bg-[#050709]">
-      <SectionHeading>Về Tôi</SectionHeading>
+      <SectionHeading>Về Tôi 1111</SectionHeading>
       <div className="w-[80%] mx-auto grid grid-cols-1 lg:grid-cols-2 gap-8 items-center mt-20">
         {/* text */}
         <div>
