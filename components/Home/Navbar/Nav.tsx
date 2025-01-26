1deab0a074214c412b0919bf93df5e64ee7fd25a
unknown <tupm@devcamp.edu.vn>
2025-01-26 20:33:42 +0700

    update

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -1,9 +1,9 @@
 "use client";
 import { navLink } from "@/constant/constant";
 import Image from "next/image";
-import Link from "next/link";
-import React, { useEffect, useState } from "react";
+import { Link as ScrollLink } from "react-scroll";
 import { HiBars3BottomRight } from "react-icons/hi2";
+import React, { useEffect, useState } from "react";
 
 type Props = {
   openNav: () => void;
@@ -11,12 +11,12 @@ type Props = {
 
 const Nav = ({ openNav }: Props) => {
   const [navBg, setNavBg] = useState(false);
+
   useEffect(() => {
     const handler = () => {
       if (window.scrollY >= 90) {
         setNavBg(true);
-      }
-      if (window.scrollY < 90) {
+      } else {
         setNavBg(false);
       }
     };
@@ -25,14 +25,15 @@ const Nav = ({ openNav }: Props) => {
       window.removeEventListener("scroll", handler);
     };
   }, []);
+
   return (
     <div
       className={`fixed ${
-        navBg ? "bg-[#240b39]" : "fixed"
+        navBg ? "bg-[#240b39]" : ""
       } h-[10vh] z-[10] w-full transition-all duration-200`}
     >
       <div className="flex items-center h-full justify-between w-[95%] sm:w-[90%] xl:w-[80%] mx-auto">
-        {/* logo */}
+        {/* Logo */}
         <Image
           src="/images/logo2.0.png"
           alt="logo"
@@ -41,30 +42,42 @@ const Nav = ({ openNav }: Props) => {
           className="ml-[-1.5rem] sm:ml-0 hover:scale-110 duration-200"
         />
 
-        {/* nav links */}
+        {/* Navigation Links */}
         <div className="flex items-center space-x-10">
           <div className="hidden lg:flex items-center space-x-8">
-            {navLink.map((navLink) => {
-              return (
-                <Link key={navLink.id} href={navLink.url}>
-                  <p className="nav_link hover:scale-110 duration-200">
-                    {navLink.label}
-                  </p>
-                </Link>
-              );
-            })}
+            {navLink.map((nav) => (
+              <ScrollLink
+                key={nav.id}
+                to={nav.url.replace("#", "")}
+                smooth={true}
+                duration={300}
+                spy={true}
+                offset={-50}
+                className="nav_link hover:scale-110 duration-200 cursor-pointer"
+              >
+                {nav.label}
+              </ScrollLink>
+            ))}
           </div>
         </div>
-        {/* button */}
+
+        {/* Contact Button & Mobile Menu */}
         <div className="flex space-x-2">
           <div className="flex items-center space-x-4">
-            <a href="#contacts">
+            <ScrollLink
+              to="contacts"
+              smooth={true}
+              duration={800}
+              offset={-50}
+              className="cursor-pointer"
+            >
               <button className="md:px-10 md:py-3 font-roboto px-8 py-3 text-blue-900 font-semibold sm:text-base text-sm bg-white hover:bg-gray-300 transition-all duration-200 rounded-lg">
                 Liên hệ với tôi
               </button>
-            </a>
+            </ScrollLink>
           </div>
-          {/* burger */}
+
+          {/* Burger Menu Icon */}
           <HiBars3BottomRight
             onClick={openNav}
             className="w-8 h-8 cursor-pointer text-white lg:hidden"
