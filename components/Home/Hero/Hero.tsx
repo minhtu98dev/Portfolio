d8d7edc14af722397c54c2db2d2ff501c6c86c7d
unknown <tupm@devcamp.edu.vn>
2025-03-20 22:57:42 +0700

    update

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -9,10 +9,8 @@ type Props = {
 
 const Hero = ({ currentLang }: Props) => {
   const info = BaseInfo[currentLang];
-  const cvFile =
-    currentLang === "vi" ? "/CV_MinhTu.pdf" : "/CV_MinhTu-ENGLISH.pdf";
-  const cvFileName =
-    currentLang === "vi" ? "CV_MinhTu.pdf" : "CV_MinhTu-ENGLISH.pdf";
+  const cvFile = currentLang === "vi" ? "/FE.pdf" : "/FE-ENL.pdf";
+  const cvFileName = currentLang === "vi" ? "FE.pdf" : "FE-ENL.pdf";
 
   return (
     <div className="w-full pt-[4vh] md:pt-[12vh] h-screen bg-[#0f0715] overflow-hidden relative">
